8bd6f178451ee619231874da82f87544702b6388
joan4741 <74694377+joan4741@users.noreply.github.com>
2021-03-05 08:22:34 -0600

    Update PPYME.php

--- PPYME.php
+++ PPYME.php
@@ -11,9 +11,9 @@ $woocommerce = new Client(
     'https://www.pymepay.com', 
     'ck_f1f7187d8c8d656cfcb0caf0a027e3090224b374', 
     'cs_49a6a046fe2d4900d482c52ea7e38d5b56db01b3',
-    //Admin
-    //'ck_00b6707129b3aeb09de7c49658181fcd2f292db9', 
-    //'cs_c3fcecd4684e3a3981efd20e3e4bebb0e1894c98',
+    
+     
+
     [
         'wp_api' => true,
         'version' => 'wc/v3',
@@ -49,7 +49,7 @@ $data = [
     ],
     'line_items' => [
         [
-            //password: e(p9JRA#t^9j
+            
             'product_id' => $_POST['codigo'],
             'name' => $_POST['producto'],
             'quantity' => $_POST['iten'],
